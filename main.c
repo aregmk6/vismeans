4acf6210c9ead2e4bc7db5e398587f45f9ff9031
aregmk6 <areg631@gmail.com>
2026-09-21 15:05:44 +0300

    working kmeans for 2 centroids, start generalizing amount of clusters that we support

--- main.c
+++ main.c
@@ -7,11 +7,11 @@
 
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
-#define RADIUS 0.2f
-
-static void handle_input(float* plane, bool* camera_flag, Camera* camera);
-static Camera initialization();
-static void loop(Camera camera);
+#define FOV 60.0f
+#define FPS 60
+#define NORMAL_RADIUS 0.2f
+#define CENTROID_RADIUS 0.4f
+#define MAX_CENTROIDS 10
 
 typedef struct {
     Vector3 pos;
@@ -19,32 +19,142 @@ typedef struct {
 } Point;
 
 typedef struct {
-    Vector3 pos;
-    Color color;
-} Centroid;
-
-typedef struct {
     Point* items;
     size_t count;
     size_t capacity;
 } Points;
 
-int main(void)
+static Camera initialization()
 {
-    Camera camera = initialization();
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dots");
 
-    loop(camera);
+    SetTargetFPS(FPS);
 
-    CloseWindow();
+    DisableCursor();
 
-    return 0;
+    return (Camera){
+        (Vector3){0.0f, 0.0f, 5.0f}, //
+        (Vector3){0.0f, 0.0f, 0.0f}, //
+        (Vector3){0.0f, 1.0f, 0.0f}, //
+        FOV,
+        CAMERA_PERSPECTIVE,
+    };
+}
+
+static void handle_input(float* plane, Color* color, bool* camera_flag,
+                         bool* point_flag, Camera* camera)
+{
+    static bool used_centroids[MAX_CENTROIDS] = {0};
+
+    if (IsKeyDown(KEY_E)) {
+        *plane -= GetFrameTime() * 5.0f;
+    }
+
+    if (IsKeyDown(KEY_Q)) {
+        *plane += GetFrameTime() * 5.0f;
+    }
+
+    if (IsKeyPressed(KEY_O)) {
+        if (IsCursorHidden()) {
+            EnableCursor();
+        } else {
+            DisableCursor();
+        }
+    }
+
+    if (IsKeyPressed(KEY_P)) {
+        if (*camera_flag)
+            *camera_flag = 0;
+        else
+            *camera_flag = 1;
+    }
+
+    // centroid
+    if (!used_centroids[0] && IsKeyPressed(KEY_ONE)) {
+        *point_flag       = 1;
+        *color            = PINK;
+        used_centroids[0] = true;
+    }
+
+    // centroid
+    if (!used_centroids[1] && IsKeyPressed(KEY_TWO)) {
+        *point_flag       = 1;
+        *color            = YELLOW;
+        used_centroids[1] = true;
+    }
+
+    if (*camera_flag) {
+        UpdateCamera(camera, CAMERA_FREE);
+    }
+}
+
+void kmeans(Points* normal_points, Points* centroids)
+{
+    if (IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+        // calculate one k-means iteration
+        struct {
+            Vector3 pos;
+            size_t size;
+        } new_cents[MAX_CENTROIDS] = {0};
+
+        for (int i = 0; i < MAX_CENTROIDS; ++i) {
+            printf("size %zu: %f, %f, %f\n", new_cents[i].size,
+                   new_cents[i].pos.x, new_cents[i].pos.y, new_cents[i].pos.z);
+        }
+
+        nob_da_foreach(Point, c, centroids)
+        {
+            size_t index         = c - centroids->items;
+            new_cents[index].pos = c->pos;
+        }
+
+        nob_da_foreach(Point, n, normal_points)
+        {
+            float min        = INT_MAX;
+            size_t min_index = 0;
+
+            nob_da_foreach(Point, c, centroids)
+            {
+                float dist = Vector3DistanceSqr(n->pos, c->pos);
+                if (dist < min) {
+                    min       = dist;
+                    min_index = c - centroids->items;
+                }
+            }
+
+            n->color = centroids->items[min_index].color;
+
+            new_cents[min_index].pos =
+                Vector3Add(new_cents[min_index].pos, n->pos);
+            new_cents[min_index].size += 1;
+        }
+
+        for (int i = 0; i < MAX_CENTROIDS; ++i) {
+            printf("size %zu: %f, %f, %f\n", new_cents[i].size,
+                   new_cents[i].pos.x, new_cents[i].pos.y, new_cents[i].pos.z);
+        }
+
+        nob_da_foreach(Point, c, centroids)
+        {
+            size_t index = c - centroids->items;
+            c->pos       = Vector3Scale(new_cents[index].pos,
+                                        1 / (float)new_cents[index].size);
+
+            printf("centroid %zu: %f, %f, %f\n", index, c->pos.x, c->pos.y,
+                   c->pos.z);
+        }
+    }
 }
 
 static void loop(Camera camera)
 {
-    Points ds   = {0};
+    Points normal_points = {0};
+    Points centroids     = {0};
+
     float plane = 0, d = 0;
-    bool camera_flag = 0;
+    bool camera_flag = 1; // one = camera on
+    bool point_flag  = 0; // zero = normal points
+    Color color      = RED;
 
     while (!WindowShouldClose()) {
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
@@ -72,12 +182,21 @@ static void loop(Camera camera)
                 ray.position.y + ray.direction.y * d,
                 plane,
             };
-            p.color = RED;
+            p.color = color;
 
-            da_append(&ds, p);
+            if (!point_flag) {
+                da_append(&normal_points, p);
+            } else {
+                da_append(&centroids, p);
+            }
+
+            point_flag = 0;
+            color      = RED;
         }
 
-        handle_input(&plane, &camera_flag, &camera);
+        handle_input(&plane, &color, &camera_flag, &point_flag, &camera);
+
+        kmeans(&normal_points, &centroids);
 
         BeginDrawing();
 
@@ -85,9 +204,14 @@ static void loop(Camera camera)
 
         BeginMode3D(camera);
 
-        nob_da_foreach(Point, p, &ds)
+        nob_da_foreach(Point, p, &normal_points)
         {
-            DrawSphere(p->pos, RADIUS, p->color);
+            DrawSphere(p->pos, NORMAL_RADIUS, p->color);
+        }
+
+        nob_da_foreach(Point, p, &centroids)
+        {
+            DrawSphere(p->pos, CENTROID_RADIUS, p->color);
         }
 
         EndMode3D();
@@ -96,47 +220,13 @@ static void loop(Camera camera)
     }
 }
 
-static Camera initialization()
+int main(void)
 {
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Dots");
+    Camera camera = initialization();
 
-    SetTargetFPS(60);
+    loop(camera);
 
-    return (Camera){
-        (Vector3){0.0f, 0.0f, 5.0f}, //
-        (Vector3){0.0f, 0.0f, 0.0f}, //
-        (Vector3){0.0f, 1.0f, 0.0f}, //
-        60.0f,
-        CAMERA_PERSPECTIVE,
-    };
-}
+    CloseWindow();
 
-static void handle_input(float* plane, bool* camera_flag, Camera* camera)
-{
-    if (IsKeyDown(KEY_E)) {
-        *plane -= GetFrameTime() * 5.0f;
-    }
-
-    if (IsKeyDown(KEY_Q)) {
-        *plane += GetFrameTime() * 5.0f;
-    }
-
-    if (IsKeyPressed(KEY_F)) {
-        if (IsCursorHidden()) {
-            EnableCursor();
-        } else {
-            DisableCursor();
-        }
-    }
-
-    if (IsKeyPressed(KEY_R)) {
-        if (camera_flag)
-            *camera_flag = 0;
-        else
-            *camera_flag = 1;
-    }
-
-    if (camera_flag) {
-        UpdateCamera(camera, CAMERA_FREE);
-    }
+    return 0;
 }
