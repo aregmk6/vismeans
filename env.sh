95b0dd5d9d307b4bc5fb5fa7e099b87c1552a198
aregmk6 <areg631@gmail.com>
2026-09-21 20:52:39 +0300

    add more freedom for controlling the pane and nvidia rendering support

---
+++ env.sh
@@ -0,0 +1,2 @@
+export __NV_PRIME_RENDER_OFFLOAD=1
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
--- main.c
+++ main.c
@@ -9,9 +9,12 @@
 #define WINDOW_HEIGHT 720
 #define FOV 60.0f
 #define FPS 60
+#define SENSITIVITY 0.1f
 #define NORMAL_RADIUS 0.2f
 #define CENTROID_RADIUS 0.4f
 #define MAX_CENTROIDS 10
+#define NOT_FOUND -10
+#define DEF_PLANE_DIST 10
 
 typedef struct {
     Vector3 pos;
@@ -33,27 +36,50 @@ static Camera initialization()
     DisableCursor();
 
     return (Camera){
-        (Vector3){0.0f, 0.0f, 5.0f}, //
-        (Vector3){0.0f, 0.0f, 0.0f}, //
-        (Vector3){0.0f, 1.0f, 0.0f}, //
+        (Vector3){0.0f, 0.0f, 10.0f}, //
+        (Vector3){0.0f, 0.0f, 0.0f},  //
+        (Vector3){0.0f, 1.0f, 0.0f},  //
         FOV,
         CAMERA_PERSPECTIVE,
     };
 }
 
-static void handle_input(float* plane, Color* color, bool* camera_flag,
-                         bool* point_flag, Camera* camera, size_t* cur_cent)
+static void my_camera_update(Camera* camera, bool camera_flag)
 {
-    static bool used_centroids[MAX_CENTROIDS] = {0};
-    static Color color_picker[]               = {
-        GRAY,  YELLOW, GOLD,     ORANGE, PINK,  MAROON,
-        GREEN, LIME,   DARKBLUE, VIOLET, BEIGE, BROWN,
-    };
+    static Vector3 movement;
+    static Vector3 rotation;
+
+    movement = (Vector3){(IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) * 0.1f -
+                             (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) * 0.1f,
+                         (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) * 0.1f -
+                             (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) * 0.1f,
+                         IsKeyDown(KEY_SPACE) * 0.1f -
+                             IsKeyDown(KEY_LEFT_CONTROL) * 0.1f};
+    rotation = (Vector3){GetMouseDelta().x * SENSITIVITY,
+                         GetMouseDelta().y * SENSITIVITY, 0.0f};
+
+    if (camera_flag) {
+        if (IsKeyDown(KEY_LEFT_SHIFT)) {
+            UpdateCameraPro(camera, Vector3Scale(movement, 2.0f), rotation,
+                            0.0f);
+        } else {
+            UpdateCameraPro(camera, movement, rotation, 0.0f);
+        }
+    }
+}
+
+static void handle_general_input(float* plane, Camera* camera)
+{
+    static bool camera_flag = 1; // one = camera on
+    static bool plane_flag  = 0; // one = plane movement on
+
+    if (plane_flag) {
+        *plane = camera->position.z - DEF_PLANE_DIST;
+    }
 
     if (IsKeyDown(KEY_E)) {
         *plane -= GetFrameTime() * 5.0f;
     }
-
     if (IsKeyDown(KEY_Q)) {
         *plane += GetFrameTime() * 5.0f;
     }
@@ -67,46 +93,30 @@ static void handle_input(float* plane, Color* color, bool* camera_flag,
     }
 
     if (IsKeyPressed(KEY_P)) {
-        if (*camera_flag)
-            *camera_flag = 0;
+        if (camera_flag)
+            camera_flag = 0;
         else
-            *camera_flag = 1;
+            camera_flag = 1;
     }
 
-    int cur_cent_int = (int)*cur_cent;
-    cur_cent_int += (int)GetMouseWheelMove();
-    if (cur_cent_int <= 0) {
-        cur_cent_int = 0;
-    } else if (cur_cent_int >= MAX_CENTROIDS - 1) {
-        cur_cent_int = MAX_CENTROIDS - 1;
-    }
-    *cur_cent = cur_cent_int;
-
-    // centroid
-    if (!used_centroids[*cur_cent] && IsKeyPressed(KEY_ONE)) {
-        *point_flag               = 1;
-        *color                    = color_picker[*cur_cent];
-        used_centroids[*cur_cent] = true;
+    if (IsKeyPressed(KEY_F)) {
+        if (plane_flag)
+            plane_flag = 0;
+        else
+            plane_flag = 1;
     }
 
-    if (*camera_flag) {
-        UpdateCamera(camera, CAMERA_FREE);
-    }
+    my_camera_update(camera, camera_flag);
 }
 
-void kmeans(Points* normal_points, Points* centroids)
+static void kmeans(Points* normal_points, Points* centroids)
 {
-    if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON)) {
+    if (IsKeyDown(KEY_T)) {
         // calculate one k-means iteration
         struct {
             Vector3 pos;
             size_t size;
         } new_cents[MAX_CENTROIDS] = {0};
-
-        for (int i = 0; i < MAX_CENTROIDS; ++i) {
-            printf("size %zu: %f, %f, %f\n", new_cents[i].size,
-                   new_cents[i].pos.x, new_cents[i].pos.y, new_cents[i].pos.z);
-        }
 
         nob_da_foreach(Point, c, centroids)
         {
@@ -154,64 +164,109 @@ void kmeans(Points* normal_points, Points* centroids)
     }
 }
 
+static Point calc_new_point(Camera* camera, Color* color, float plane)
+{
+
+    /*
+     *  draw only on the Z = 0 plane.
+     *  d = (P0 - l0)*n / l*n
+     *  where:
+     *  P0 = (0, 0, z)
+     *  l0 = ray.position
+     *  l = ray.direction
+     *  n = (0,0,1)
+     */
+
+    float d = 0;
+    Point p = {0};
+    Ray ray = {camera->position, Vector3Normalize(Vector3Subtract(
+                                     camera->target, camera->position))};
+
+    if (ray.direction.z != 0) {
+        d = (plane - ray.position.z) / ray.direction.z;
+    }
+
+    p.pos = (Vector3){
+        ray.position.x + ray.direction.x * d,
+        ray.position.y + ray.direction.y * d,
+        plane,
+    };
+    p.color = *color;
+    return p;
+}
+
+static void pick_centroid(int* cur_cent, bool* used_centroids)
+{
+    if (*cur_cent != NOT_FOUND) {
+        *cur_cent += (int)GetMouseWheelMove();
+
+        if (*cur_cent <= 0) {
+            *cur_cent = 0;
+        } else if (*cur_cent >= MAX_CENTROIDS - 1) {
+            *cur_cent = MAX_CENTROIDS - 1;
+        }
+        if (used_centroids[*cur_cent]) {
+            // printf("cent %d is used, checking for unused:\n", cur_cent);
+            for (int i = (*cur_cent + 1) % MAX_CENTROIDS; i != *cur_cent;
+                 i     = (i + 1) % MAX_CENTROIDS) {
+                if (!used_centroids[i]) {
+                    // printf("FOUND! cent %d is unused\n", i);
+                    *cur_cent = i;
+                    break;
+                }
+                // printf("cent %d is also used, move on...\n", i);
+                if (i == *cur_cent - 1) {
+                    printf("NOT FOUND! there are no unused cents!\n");
+                    *cur_cent = NOT_FOUND;
+                    break;
+                }
+            }
+        }
+    }
+}
+
 static void loop(Camera camera)
 {
+    static bool used_centroids[MAX_CENTROIDS] = {0}; // zero = unused
+    static Color color_picker[]               = {
+        GRAY, YELLOW,   ORANGE, PINK,  MAROON,
+        LIME, DARKBLUE, VIOLET, BEIGE, BROWN,
+    };
+    static const char* color_names[] = {
+        "GRAY", "YELLOW",   "ORANGE", "PINK",  "MAROON",
+        "LIME", "DARKBLUE", "VIOLET", "BEIGE", "BROWN",
+    };
+    static Color default_color = RED;
 
     Points normal_points = {0};
     Points centroids     = {0};
 
-    float plane = 0, d = 0;
-    bool camera_flag = 1; // one = camera on
-    bool point_flag  = 0; // zero = normal points
-    Color color      = RED;
-    size_t cur_cent  = 0;
+    float plane  = 0;
+    int cur_cent = 0;
 
     while (!WindowShouldClose()) {
+
+        pick_centroid(&cur_cent, used_centroids);
+
         if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
-
-            /*
-             *  draw only on the Z = 0 plane.
-             *  d = (P0 - l0)*n / l*n
-             *  where:
-             *  P0 = (0, 0, z)
-             *  l0 = ray.position
-             *  l = ray.direction
-             *  n = (0,0,1)
-             */
-
-            Point p = {0};
-            Ray ray = {camera.position, Vector3Normalize(Vector3Subtract(
-                                            camera.target, camera.position))};
-
-            if (ray.direction.z != 0) {
-                d = (plane - ray.position.z) / ray.direction.z;
-            }
-
-            p.pos = (Vector3){
-                ray.position.x + ray.direction.x * d,
-                ray.position.y + ray.direction.y * d,
-                plane,
-            };
-            p.color = color;
-
-            if (!point_flag) {
-                da_append(&normal_points, p);
-            } else {
-                da_append(&centroids, p);
-            }
-
-            point_flag = 0;
-            color      = RED;
+            da_append(&normal_points,
+                      calc_new_point(&camera, &default_color, plane));
         }
 
-        handle_input(&plane, &color, &camera_flag, &point_flag, &camera,
-                     &cur_cent);
+        if (cur_cent != NOT_FOUND && !used_centroids[cur_cent] &&
+            IsMouseButtonPressed(MOUSE_RIGHT_BUTTON)) {
+            used_centroids[cur_cent] = true;
+            da_append(&centroids,
+                      calc_new_point(&camera, &color_picker[cur_cent], plane));
+        }
+
+        handle_general_input(&plane, &camera);
 
         kmeans(&normal_points, &centroids);
 
         BeginDrawing();
 
-        ClearBackground(BLACK);
+        ClearBackground(WHITE);
 
         BeginMode3D(camera);
 
@@ -227,8 +282,12 @@ static void loop(Camera camera)
 
         EndMode3D();
 
-        DrawText(TextFormat("Current Centroid: %03i", cur_cent), 10, 10, 20,
-                 RED);
+        if (cur_cent != NOT_FOUND) {
+            DrawText(TextFormat("Current Centroid: %s", color_names[cur_cent]),
+                     10, 10, 20, RED);
+        } else {
+            DrawText("Out of Centroids", 10, 10, 20, RED);
+        }
 
         EndDrawing();
     }
